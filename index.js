51e54b95399455ba9b87a0b8ff3bfced10ee532e
Vikas Yadav <vikas.yadav_cs18@gla.ac.in>
2021-02-02 16:54:14 +0530

    go

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const buf_data=fs.readFileSync("read.txt");
 
 
 console.log(buf_data.toString());
+
+// asynchronously creating folder and file--------------
+
+// fs.mkdirSync("nfolder");
+// fs.writeFile("nfolder/newfile.txt","hey brother",(err)=>{
+//     console.log("file created");
+
+// });
+
+fs.renameSync("updatedread.txt","read.txt");
